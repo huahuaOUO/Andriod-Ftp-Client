a5ca18e62dffc4201e05acf852441d0ac9902505
mfgpker <ipodlikeme@gmail.com>
2014-03-28 14:20:54 +0100

    fransk

--- src/com/mfgpker/ftpclient/VideoPlayer.java
+++ src/com/mfgpker/ftpclient/VideoPlayer.java
@@ -8,19 +8,48 @@
  */
 package com.mfgpker.ftpclient;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class VideoPlayer extends Activity{
+public class VideoPlayer extends Activity implements OnCompletionListener, OnPreparedListener{
 
-	private String filename;
+	private String filename, TAG = "VideoPlayer";
 	
+	private VideoView videoPlayer;
+	//private MediaController ctlr;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.vidoe);
-		
 		Bundle gotBasket = getIntent().getExtras();
 		filename = gotBasket.getString("name");
+		videoPlayer = (VideoView) findViewById(R.id.videoView);
+		videoPlayer.setOnPreparedListener(this);
+        videoPlayer.setOnCompletionListener(this);
+        videoPlayer.setKeepScreenOn(true); 
+
+        File root = Environment.getExternalStorageDirectory(); 
+        Log.i(TAG, "Root external storage="+root);
+        
+        
+	}
+
+	public void onPrepared(MediaPlayer mp) {
+		
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
 		
 	}
 }
