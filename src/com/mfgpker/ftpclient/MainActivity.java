16dc0eded2550f53f6db8dc3bc8cac8cb2458e56
mfgpker <ipodlikeme@gmail.com>
2014-03-30 18:48:16 +0200

    profil save

--- src/com/mfgpker/ftpclient/MainActivity.java
+++ src/com/mfgpker/ftpclient/MainActivity.java
@@ -25,15 +25,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener, OnItemSelectedListener {
-	 public static final String PREFS_NAME = "FTPINFOPrefsFile";
+	public static final String PREFS_NAME = "FTPINFOPrefsFile";
 
 	TextView ip, port, username, password;
 	String ips, ports, usernames, passwords;
+	int modes;
 	String TAG = "MainActivity";
 	CheckBox save;
+	Spinner profil_spinner, protocol_spinner;
+
 	private enum Mode {
 		FTP, SFTP
 	}
+
+	int saveprofil = 0;
 
 	private Mode mode = Mode.FTP;
 
@@ -55,27 +60,60 @@ public class MainActivity extends Activity implements OnClickListener, OnItemSel
 			Toast.makeText(MainActivity.this, res, Toast.LENGTH_LONG).show();
 
 		}
-		
+		String savepro = String.valueOf(saveprofil);
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-	    ips = settings.getString("ip", "");
-	    ports = settings.getString("port", "21");
-	    usernames = settings.getString("user", "");
-	    passwords = settings.getString("pass", "");
+		ips = settings.getString("ip" + savepro, "");
+		ports = settings.getString("port" + savepro, "21");
+		usernames = settings.getString("user" + savepro, "");
+		passwords = settings.getString("pass" + savepro, "");
+		modes = settings.getInt("mode" + savepro, 0);
+		mode = (modes == 0) ? Mode.FTP : Mode.SFTP;
 		
-	    ip.setText(ips);
-	    port.setText(ports);
-	    username.setText(usernames);
-	    password.setText(passwords);
-	    
-		Spinner spinner = (Spinner) findViewById(R.id.connection_protocol);
+		ip.setText(ips);
+		port.setText(ports);
+		username.setText(usernames);
+		password.setText(passwords);
+
+		
+
+		protocol_spinner = (Spinner) findViewById(R.id.connection_protocol);
 		// Create an ArrayAdapter using the string array and a default spinner
 		// layout
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.connection_protocol_array, android.R.layout.simple_spinner_item);
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(this);
+		protocol_spinner.setAdapter(adapter);
+		protocol_spinner.setOnItemSelectedListener(this);
+		
+		
+		profil_spinner = (Spinner) findViewById(R.id.spinnerSaveinfo);
+		ArrayAdapter<CharSequence> adapterpr = ArrayAdapter.createFromResource(this, R.array.Saveprofil_array, android.R.layout.simple_spinner_item);
+		adapterpr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		profil_spinner.setAdapter(adapterpr);
+		profil_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			public void onItemSelected(AdapterView<?> parent, View v, int pos, long id) {
+				saveprofil = pos;
+				Log.d(TAG, "profil " + saveprofil);
+				
+				String savepro = String.valueOf(saveprofil);
+				SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+				ips = settings.getString("ip" + savepro, "");
+				ports = settings.getString("port" + savepro, "21");
+				usernames = settings.getString("user" + savepro, "");
+				passwords = settings.getString("pass" + savepro, "");
+				modes = settings.getInt("mode" + savepro, 0);
+				mode = (modes == 0) ? Mode.FTP : Mode.SFTP;
+				
+				ip.setText(ips);
+				port.setText(ports);
+				username.setText(usernames);
+				password.setText(passwords);
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {}
+		});
 
 	}
 
@@ -87,19 +125,21 @@ public class MainActivity extends Activity implements OnClickListener, OnItemSel
 			String port = this.port.getText().toString();
 			String user = this.username.getText().toString();
 			String pass = this.password.getText().toString();
-
-			if(save.isChecked()){
+			String savepro = String.valueOf(saveprofil);
+			Log.d(TAG, savepro);
+			if (save.isChecked()) {
 				SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-			      SharedPreferences.Editor editor = settings.edit();
-			      editor.putString("ip", ip);
-			      editor.putString("port", port);
-			      editor.putString("user", user);
-			      editor.putString("pass", pass);
-			      // Commit the edits!
-			      editor.commit();
-			      Log.d(TAG, "Saved!");
+				SharedPreferences.Editor editor = settings.edit();
+				editor.putString("ip" + savepro, ip);
+				editor.putString("port" + savepro, port);
+				editor.putString("user" + savepro, user);
+				editor.putString("pass" + savepro, pass);
+				editor.putInt("mode" + savepro, modes);
+				// Commit the edits!
+				editor.commit();
+				Log.d(TAG, "Saved!");
 			}
-			
+
 			// new Login().execute(ip, port, user, pass);
 			Bundle basket = new Bundle();
 			basket.putString("ip", ip);
@@ -107,30 +147,29 @@ public class MainActivity extends Activity implements OnClickListener, OnItemSel
 			basket.putString("user", user);
 			basket.putString("pass", pass);
 			Intent i;
-			if(mode == Mode.FTP){
+			if (mode == Mode.FTP) {
 				i = new Intent(MainActivity.this, Ftp.class);
 				i.putExtras(basket);
 				startActivity(i);
-			}
-			else if (mode == Mode.SFTP){
+			} else if (mode == Mode.SFTP) {
 				i = new Intent(MainActivity.this, Sftp.class);
 				i.putExtras(basket);
 				startActivity(i);
 			}
-			
+
 			break;
 
 		}
 
 	}
 
-	public void onItemSelected(AdapterView<?> av, View v, int pos, long id) {
+	public void onItemSelected(AdapterView<?> parent, View v, int pos, long id) {
 		mode = (pos == 0) ? Mode.FTP : Mode.SFTP;
 		Log.d(TAG, mode.toString());
+		
 	}
 
 	public void onNothingSelected(AdapterView<?> arg0) {
-
 	}
 
 }
--- src/com/mfgpker/ftpclient/Sftp.java
+++ src/com/mfgpker/ftpclient/Sftp.java
@@ -8,33 +8,53 @@
  */
 package com.mfgpker.ftpclient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
-public class Sftp extends Activity implements OnClickListener {
+public class Sftp extends Activity implements OnClickListener, OnItemClickListener, OnItemLongClickListener {
 
 	Session session = null;
 	Channel channel = null;
 	ChannelSftp channelSftp = null;
 	JSch jsch = null;
+	private static String TAG = "SFTP";
+
+	private List<Content> myContents = new ArrayList<Content>();
 
 	String SFTPHOST;
 	int SFTPPORT;
 	String SFTPUSER;
 	String SFTPPASS;
-	String SFTPWORKINGDIR;
+	String workingDir, orginalDir;
 
-	Button disc;
+	private Button btnUpload, btnDisconnect, btnContent;
+
+	private ListView contentList;
+	private TextView txtPath;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,12 +63,22 @@ public class Sftp extends Activity implements OnClickListener {
 		Bundle gotBasket = getIntent().getExtras();
 		SFTPHOST = gotBasket.getString("ip");
 		String port = gotBasket.getString("port");
-		SFTPPORT = Integer.parseInt(port.isEmpty()? "22" : port);
+		SFTPPORT = Integer.parseInt(port.isEmpty() ? "22" : port);
 		SFTPUSER = gotBasket.getString("user");
 		SFTPPASS = gotBasket.getString("pass");
 
-		disc = (Button) findViewById(R.id.disconnect);
-		disc.setOnClickListener(this);
+		btnUpload = (Button) findViewById(R.id.upload);
+		btnDisconnect = (Button) findViewById(R.id.disconnect);
+		btnContent = (Button) findViewById(R.id.getContent);
+		txtPath = (TextView) findViewById(R.id.txtPath);
+
+		btnDisconnect.setOnClickListener(this);
+		btnUpload.setOnClickListener(this);
+		btnContent.setOnClickListener(this);
+
+		contentList = (ListView) findViewById(R.id.contentList);
+		contentList.setOnItemClickListener(this);
+		contentList.setOnItemLongClickListener(this);
 
 		ConnectAndLogin();
 	}
@@ -70,8 +100,9 @@ public class Sftp extends Activity implements OnClickListener {
 
 					channelSftp = (ChannelSftp) channel;
 
-					SFTPWORKINGDIR = channelSftp.getHome();
-					channelSftp.cd(SFTPWORKINGDIR);
+					workingDir = channelSftp.getHome();
+					orginalDir = workingDir;
+					channelSftp.cd(workingDir);
 					System.out.println(channelSftp.getHome());
 					// channelSftp.get("remotefile.txt", "localfile.txt");
 					// File f = new File(FILETOTRANSFER);
@@ -96,11 +127,83 @@ public class Sftp extends Activity implements OnClickListener {
 
 	public void onClick(View v) {
 		switch (v.getId()) {
+		case R.id.upload:
+			break;
+		case R.id.getContent:
+			updateList();
+			break;
+
 		case R.id.disconnect:
 			Disconnect();
-			Log.d("guirehgreu" , "edfewfrefgrewgtrwgtrw");
+			Log.d("guirehgreu", "edfewfrefgrewgtrwgtrw");
 			break;
 		}
+	}
+
+	public boolean onItemLongClick(AdapterView<?> parent, View v, int pos, long id) {
+		// TODO Auto-generated method stub
+		Content cont = myContents.get(pos);
+		return true;
+	}
+
+	public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+		// TODO Auto-generated method stub
+		Content cont = myContents.get(pos);
+	}
+
+	private void updateList() {
+		getContent();
+		txtPath.setText("Path: " + workingDir);
+		ArrayAdapter<Content> adapter = new MyListAdapter();
+		contentList.setAdapter(adapter);
+		Toast.makeText(Sftp.this, "Updated", Toast.LENGTH_SHORT).show();
+	}
+
+	private void getContent() {
+		// TODO: get name of files and folders
+		try {
+			Vector filelist = channelSftp.ls(workingDir);
+			for (int i = 0; i < filelist.size(); i++) {
+				Log.d(TAG, filelist.get(i).toString());
+			}
+		} catch (SftpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private class MyListAdapter extends ArrayAdapter<Content> {
+
+		public MyListAdapter() {
+			super(Sftp.this, R.layout.item_view, myContents);
+
+		}
+
+		public View getView(int pos, View convertView, ViewGroup parent) {
+			View itemView = convertView;
+			if (itemView == null) {
+				itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
+			}
+
+			// find car work with,
+			Content currentcont = myContents.get(pos);
+
+			// fill the view
+			// icon
+			ImageView image = (ImageView) itemView.findViewById(R.id.item_icon);
+			image.setImageResource(currentcont.getIconID());
+
+			// name
+			TextView maketext = (TextView) itemView.findViewById(R.id.item_txtName);
+			maketext.setText(currentcont.getName());
+
+			// size
+			TextView conditiontext = (TextView) itemView.findViewById(R.id.item_txtsize);
+			conditiontext.setText("" + currentcont.getSize());
+
+			return itemView;
+		}
+
 	}
 
 }
