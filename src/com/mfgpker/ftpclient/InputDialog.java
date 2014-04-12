15be75911599fcc8eb5b49c3f0b39713b5d5672a
mfgpker <ipodlikeme@gmail.com>
2014-04-12 17:06:13 +0200

    alertdialog

---
+++ src/com/mfgpker/ftpclient/InputDialog.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.mfgpker.ftpclient;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * @author frank
+ *
+ */
+public class InputDialog extends Dialog{
+
+	public InputDialog(Context context) {
+		super(context);
+	}
+	
+	
+
+}
